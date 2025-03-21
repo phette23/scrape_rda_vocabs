6ab2fe9c055f29e5d649dc12265ed28d90a9d1ec
phette23 <phette23@gmail.com>
2025-03-21 12:21:14 -0700

    fix: add AV category sql & fix muliple other carriers

--- koha-sql.js
+++ koha-sql.js
@@ -5,15 +5,24 @@ import vocabs from './vocabs.json' with {type: 'json'}
 const INSERT = 'INSERT INTO authorised_values (category, authorised_value, lib, lib_opac)\nVALUES '
 let sql = ''
 
-const termSql = (category, type) => {
+const termSQL = (category, type) => {
+    sql += `INSERT INTO authorised_value_categories (category_name) VALUES ('${category}');\n`
     sql += INSERT
     type.terms.forEach((term, index) => {
+        // skip Carrier "other" terms which have the same term text but different codes by carrier category
+        if (category.match("CARRIER") && term.match("other")) return
         sql += `('${category}', '${term}', '${term}', '${term}')`
-        sql += index + 1 === type.terms.length ? ';\n' : ',\n'
+        if (index + 1 === type.terms.length) {
+            if (category.match("CARRIER")) sql += `,\n('${category}', 'other', 'other', 'other')`
+            sql += ';\n'
+        } else {
+            sql += ',\n'
+        }
     })
 }
 // make the text descriptions of codes their corresponding terms
-const codeSql = (category, type) => {
+const codeSQL = (category, type) => {
+    sql += `INSERT INTO authorised_value_categories (category_name) VALUES ('${category}');\n`
     sql += INSERT
     type.codes.forEach((term, index) => {
         sql += `('${category}', '${term}', '${type.terms[index]}', '${type.terms[index]}')`
@@ -21,12 +30,12 @@ const codeSql = (category, type) => {
     })
 }
 
-termSql("RDACARRIER", vocabs.carrier)
-codeSql("RDACARRIER_CODE", vocabs.carrier)
-termSql("RDACONTENT", vocabs.content)
-codeSql("RDACONTENT_CODE", vocabs.content)
-termSql("RDAMEDIA", vocabs.media)
-codeSql("RDAMEDIA_CODE", vocabs.media)
+termSQL("RDACARRIER", vocabs.carrier)
+codeSQL("RDACARRIER_CODE", vocabs.carrier)
+termSQL("RDACONTENT", vocabs.content)
+codeSQL("RDACONTENT_CODE", vocabs.content)
+termSQL("RDAMEDIA", vocabs.media)
+codeSQL("RDAMEDIA_CODE", vocabs.media)
 
 console.log(sql)
 fs.writeFile('koha.sql', sql, err => {
